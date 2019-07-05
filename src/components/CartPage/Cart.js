384eee26da6cc2d2238fb2d76371c9556f667db6
Nawaz Ahmad <ahmadnawazgct@gmail.com>
2019-07-05 09:24:34 -0700

    first commit

--- src/components/CartPage/Cart.js
+++ src/components/CartPage/Cart.js
@@ -4,19 +4,19 @@ import CartList from '../CartPage/CartList';
 import CartColumns from '../CartPage/CartColumns';
 import CartTotals from '../CartPage/CartTotals';
 
-export default function Cart() {
+export default function Cart({history}) {
     return (
       <section className="py-5">
         <div className="container">
           {/*title */}
-            <Title title="your cart items" center="true" />
+          <Title title="your cart items" center="true" />
         </div>
         {/* cart columns */}
         <CartColumns />
         {/* cart list */}
         <CartList />
         {/* cart totals */}
-        <CartTotals />
+        <CartTotals history={history} />
       </section>
     );
 }
--- src/components/CartPage/CartTotals.js
+++ src/components/CartPage/CartTotals.js
@@ -1,33 +1,36 @@
 import React from 'react';
 import { ProductConsumer } from '../context';
+import PayPalBtn from '../CartPage/PayPallBtn';
 
-export default function CartTotals() {
+export default function CartTotals({ history }) {
     return (
-       
       <div className="container">
         <div children="row">
-            <ProductConsumer>
-                {value => {
-                const { cartTotal, cartSubTotal, clearCart, cartTax } = value;
-                return (
-                <div className="col text-center text-title my-4">
-                    <button
-                    type="button"
-                    className="btn btn-outline-danger text-capitalize mb-4"
-                    onClick={clearCart}
-                    >
-                    clear cart
-                    </button>
-                    <h3>subtotal: ${cartSubTotal}</h3>
-                    <h3>tax: ${cartTax}</h3>
-                    <h3>total: ${cartTotal}</h3>
-                </div>
-                );
-            }}
-            
-            </ProductConsumer>
-            
+          <ProductConsumer>
+          {value => {
+          const {cartTotal,cartSubTotal,clearCart,cartTax} = value;
+            return (
+              <div className="col text-center text-title my-4">
+                <button
+                  type="button"
+                  className="btn btn-outline-danger text-capitalize mb-4"
+                  onClick={clearCart}
+                >
+                clear cart
+                </button>
+                <h3>subtotal: ${cartSubTotal}</h3>
+                <h3>tax: ${cartTax}</h3>
+                <h3>total: ${cartTotal}</h3>
+                <PayPalBtn
+                  history={history}
+                  cartTotal={cartTotal}
+                  clearCart={clearCart}
+                />
             </div>
-        </div>
-    );
+          );
+        }}
+      </ProductConsumer>
+    </div>
+  </div>
+  );
 }
